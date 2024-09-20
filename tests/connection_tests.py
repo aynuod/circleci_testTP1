bcd5795d2bf6ca6214ad44b27f5f789510f4edcf
imadzajli <imadzajli6@gmail.com>
2024-09-20 22:53:54 +0100

    working ?

--- tests/connection_tests.py
+++ tests/connection_tests.py
@@ -4,12 +4,12 @@ def pas():
     return 1
 
 
-with open("tests.json",'r') as file:
+with open("tests/tests.json",'r') as file:
     data = json.load(file)
 
 data["connection_test"] = "passed"
 
-with open("tests.json",'w') as file:
+with open("tests/tests.json",'w') as file:
     json.dump(data,file,indent=1)
 
 print(pas())
